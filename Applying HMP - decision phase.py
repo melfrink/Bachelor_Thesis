23dd6305526355cad73ae24ef7deedb0176fed03
melfrink <150018100+melfrink@users.noreply.github.com>
2026-06-22 16:58:40 +0200

    final files applying HMP
    both for the decision phase and response phase, small revisements on the latter.

--- Applying HMP - decision phase.py
+++ Applying HMP - decision phase.py
@@ -2,7 +2,7 @@
 """
 Created on Sat May 30 2026
 
-@author: Mathies Elfrink, adapted from 'Applying HMP - final'
+@author: Mathies Elfrink, adapted from 'Applying HMP - response phase' 
 
 Applying HMP on the decision phase
 """
@@ -24,8 +24,9 @@ import numpy as np
 import pickle
 
 from pathlib import Path
-# %%
 
+
+# %%
 """
 preprocessing 1: loading data, splitsing player 1 from player 2
 """
@@ -164,22 +165,38 @@ epoch_data_draw = xr.concat(
     dim='participant'
 )
 
+#%%
+"""
+preprocessing 4: concat all for the collapsed model
+"""
+epoch_data_collapsed = xr.concat([epoch_data_win, epoch_data_loss, epoch_data_draw], dim='epoch')
+
+# Reset epoch index to be unique - necessary for HMP
+epoch_data_collapsed = epoch_data_collapsed.assign_coords(
+    epoch=np.arange(epoch_data_collapsed.sizes['epoch'])
+)
+
 
 # %%
-
 """
-fitting the model for all three datasets - 
+reset models
 """
 results = {}
 models = {}
+#%%
 
-for label, data in [('win', epoch_data_win), ('loss', epoch_data_loss), ('draw', epoch_data_draw)]:
-    # Step 1: Preprocess (set n_comp manually, more is more detailed, therefore also more likely to introduce noise)
-    # preprocessed = hmp.preprocessing.Standard(epoch_data, n_comp=32, apply_standard=True)
-    preprocessed = hmp.transformers.ProjPCA(data, n_comp=32, interval_id='rt', offset_end=0, reject_threshold=1e-4, min_duration=0.2, max_duration=2.0) # EDIT was: offset_end = 0.05 or 0.1, 'worked' with =0, -0.1 did not give better results
+"""
+applying HMP: fitting the model for the three conditions
+"""
+
+
+for label, data in [('win_1000_EventModel_n=3_sp=8', epoch_data_win), ('loss_1000_EventModel_n=3_sp=8', epoch_data_loss), ('draw_1000_EventModel_n=3_sp=8', epoch_data_draw)]:
+    # Step 1: Preprocess using PCA
+    # >> set ofset_end to change RT
+    preprocessed = hmp.transformers.ProjPCA(data, n_comp=32, interval_id='rt', offset_end=-1.0, reject_threshold=1e-4, min_duration=0.2, max_duration=2.0) 
 
     # Step 2: Create expected pattern
-    pattern = hmp.patterns.HalfSine.create_expected(sfreq=epoch_data.sfreq, width=50, location=50)
+    pattern = hmp.patterns.HalfSine.create_expected(sfreq=epoch_data_win.sfreq, width=50, location=50)
         
     # Step 3: Build TrialData
     trial_data = hmp.trialdata.TrialData.from_transformer(preprocessed, pattern=pattern.template)
@@ -187,18 +204,48 @@ for label, data in [('win', epoch_data_win), ('loss', epoch_data_loss), ('draw',
     # Step 4: Fit model 
     #   a) EventModel - set number of events 
     #   b) CumulativeMethod - let HMP figure out this number (here: resulted in duplicates)
-    n_events = 4
-    model = hmp.models.EventModel(pattern, n_events=n_events, starting_points=1, max_scale=10) #edit: starting_points was 10, max_scale was 51. Worked with startingpoints=1, max_scale=10. With starting_points=3, max_scale=20 too much. 
-    #model = hmp.models.CumulativeMethod(pattern, max_n_events=4) # EDIT: max_n_events toegevoegd
+    n_events = 3
+    model = hmp.models.EventModel(pattern, n_events=n_events, starting_points=8, max_scale=41) # max_scale = (n_events + 1) * 10 + 1
+    #model = hmp.models.CumulativeMethod(pattern) 
+    _, estimates = model.fit_transform(trial_data)
+    
+    results[label] = (estimates) # estimates for showing end result
+    #models[label] = model # model for showing log-likelihood plot
+
+
+# %%
+
+"""
+applying HMP: fitting the model for the collapsed dataset
+"""
+
+for label, data in [('collapsed_1000_EventModel_n=3_sp=8', epoch_data_collapsed)]:
+    # Step 1: Preprocess using PCA
+    # >> set ofset_end to change RT
+    preprocessed = hmp.transformers.ProjPCA(data, n_comp=32, interval_id='rt', offset_end=-1.0, reject_threshold=1e-4, min_duration=0.2, max_duration=2.0) 
+
+    # Step 2: Create expected pattern
+    pattern = hmp.patterns.HalfSine.create_expected(sfreq=epoch_data_collapsed.sfreq, width=50, location=50)
+        
+    # Step 3: Build TrialData
+    trial_data = hmp.trialdata.TrialData.from_transformer(preprocessed, pattern=pattern.template)
+
+    # Step 4: Fit model 
+    #   a) EventModel - set number of events 
+    #   b) CumulativeMethod - let HMP figure out this number (here: resulted in duplicates)
+    n_events = 3
+    model = hmp.models.EventModel(pattern, n_events=n_events, starting_points=8, max_scale=41) # max_scale = (n_events + 1) * 10 + 1
+    #model = hmp.models.CumulativeMethod(pattern)
     _, estimates = model.fit_transform(trial_data)
     
     results[label] = (estimates) # estimates for showing end result
     models[label] = model # model for showing log-likelihood plot
-
+    
+    
 
 #%%
 """
-Analyse data - getting average event timings
+Analyse data - getting average event timings -> conditions (similar for collapsed)
 """
 
 timings = {}
@@ -208,48 +255,99 @@ for label in ['win', 'loss', 'draw']:
     rounded_timing = np.round(timing.values / 5) * 5 # round similar to the cumulative method
     timings[label] = rounded_timing
 
+
+#%%
+
+"""
+visualize data - topographies -> conditions
+"""
+
+# set working dir (not sure why this is necessary, maybe for by read_info())
+os.chdir('C:/Scriptie_Data/rps') 
+
+info = read_info(all_files[0], verbose=False)
+
+all_epoch_data = {
+    'win':  epoch_data_win,
+    'loss': epoch_data_loss,
+    'draw': epoch_data_draw,
+}
+
+suffixes = list(dict.fromkeys(k.split('_', 1)[1] for k in results))
+
+for suffix in suffixes:
+    for outcome in ['win', 'loss', 'draw']:
+        key = f"{outcome}_{suffix}"
+        if key in results:
+            hmp.visu.plot_topo_timecourse(
+                all_epoch_data[outcome],
+                results[key],
+                info,
+                as_time=True,
+                event_lines=False,
+                title=f"Topographical Timecourse: {outcome.capitalize()} {suffix}",
+                max_time=1000, # fix topography-axis
+                vmin = -1e-5, # fix max and min EEG signal
+                vmax = 1e-5
+            )
+
 #%%
 """
-visualize data - topographies
+visualize data - topographies -> collapsed
 """
 # set working dir (not sure why this is necessary, maybe for by read_info())
 os.chdir('C:/Scriptie_Data/rps') 
 
 info = read_info(all_files[0], verbose=False)
-hmp.visu.plot_topo_timecourse(epoch_data_win, results['win'], info, as_time=True, event_lines=False, title="A. Topographical Timecourse: Win")
-hmp.visu.plot_topo_timecourse(epoch_data_loss, results['loss'], info, as_time=True, event_lines=False, title="B. Topographical Timecourse: Loss")
-hmp.visu.plot_topo_timecourse(epoch_data_draw, results['draw'], info, as_time=True, event_lines=False, title="C. Topographical Timecourse: Draw")
-
-
-#%%
-"""
-visualize data - timeline average probability for each event at each time sample
-"""
-
-for event in range(7):
-    plt.plot(estimates.sel( event=event).mean('trial'), label=f"Event{event+1}")
-plt.ylabel('P(event)')
-plt.xlabel('Time (sample)')
-plt.legend()
-plt.show()
-
-#%%
-"""
-visualize data - timeline probability for each event for one trial
-"""
-
-for event in range(7):
-    plt.plot(estimates.sel(trial=('decision_phase_sub-01_player-1_epo',0), event=event), label=f"Event {event+1}") # TEST: 'TEST_'
-plt.ylabel('P(event)')
-plt.xlabel('Time (sample)')
-plt.xlim(0,400)
-plt.legend()
-plt.show()
+hmp.visu.plot_topo_timecourse(epoch_data_collapsed, 
+                              results['collapsed_1000_EventModel_n=3_sp=8'], 
+                              info, 
+                              as_time=True, 
+                              event_lines=False, 
+                              title="Topographical Timecourse: Collapsed",
+                              max_time=1000,
+                              vmin = -1e-5, 
+                              vmax = 1e-5)
 
 
 
 #%%
-x = model
+"""
+visualize data - timeline average probability for each event in ms -> conditions
+"""
+
+
+# set working dir (not sure why this is necessary, maybe for by read_info())
+os.chdir('C:/Scriptie_Data/rps') 
+
+info = read_info(all_files[0], verbose=False)
+
+all_epoch_data = {
+    'win':  epoch_data_win,
+    'loss': epoch_data_loss,
+    'draw': epoch_data_draw,
+}
+
+suffixes = list(dict.fromkeys(k.split('_', 1)[1] for k in results))
+
+for suffix in suffixes:
+    for outcome in ['win', 'loss', 'draw']:
+        key = f"{outcome}_{suffix}"
+        if key in results:
+            for event in range(3):
+                estimates = results[key]
+                data = estimates.sel(event=event).mean('trial')
+                time_ms = data.sample.values / sfreq * 1000  # samples -> ms
+                plt.plot(time_ms, data.values, label=f"Event {event+1}")
+
+            plt.ylabel('P(event)')
+            plt.ylim(top=0.025)
+            plt.xlabel('Time (ms)')
+            plt.title(f'Average Probability Distributions: {outcome.capitalize()}')
+            plt.legend()
+            plt.show()
+
+
 
 #%%
 """
@@ -258,7 +356,7 @@ log-likelihood plot
 # setting the plot
 fig, axes = plt.subplots(1, 3, figsize=(15, 4), sharey=False)
 
-# setting the colors
+# setting the colors... because why not pick some nice colors :)
 colors = {'win': 'steelblue', 'loss': 'tomato', 'draw': 'goldenrod'}
 
 
@@ -284,7 +382,7 @@ for ax, label in zip(axes, ['win', 'loss', 'draw']):
 
     ax.plot(n_events_vals, ll_vals, 'o-', color=colors[label], linewidth=2, markersize=6)
 
-    # Mark the last number of events for clarity 
+    # Mark the final number of events for clarity 
     final_n = model.submodels[-1].n_events
     final_ll = model.submodels[-1].lkhs[0]
     ax.plot(final_n, final_ll, 'o', color=colors[label], markersize=12,
@@ -296,20 +394,28 @@ for ax, label in zip(axes, ['win', 'loss', 'draw']):
     ax.legend(fontsize=10)
     ax.grid(True, alpha=0.3)
 
-plt.suptitle('Log-likelihood per conditie (CumulativeMethod)', fontsize=14, y=1.02)
+plt.suptitle('Log-likelihood per conditie (CumulativeMethod)', fontsize=14, y=1.0)
 plt.tight_layout()
 plt.savefig('loglikelihood_per_conditie.png', dpi=150, bbox_inches='tight')
 plt.show()
-# %%
 
-"""
-save estimates as pickle files
-"""
 
-for label, estimates in results.items():
-    with open(f"estimates_{label}.pkl", 'wb') as output:
-        pickle.dump(estimates, output)
-        
+#%%
+"""
+save single estimates as pickle
+"""
+with open("collapsed_1000_EventModel_n=6_sp=8.pkl", 'wb') as output:
+    pickle.dump(estimates, output)
+    
+#%%
+"""
+open single estimates pickle
+"""
+epoch_data_path = 'C:/Scriptie_Data/rps' # current wd
+
+with open("C:/Scriptie_Data/rps/collapsed_1000_EventModel_n=3_sp=8.pkl", 'rb') as output:
+    results['collapsed_1000_EventModel_n=3_sp=8'] = pickle.load(output)
+    
 # %%
 
 """
@@ -322,7 +428,7 @@ for label, model in models.items():
         pickle.dump(model, output)
 #%%
 """
-loading pickled model and estimates
+loading pickled models and estimates
 """
 
 rps_path = Path('C:/Scriptie_Data/rps')
@@ -336,100 +442,8 @@ for filepath in rps_path.iterdir():
     
 results = {}
 for filepath in rps_path.iterdir():
-    if filepath.stem.startswith('estimates_'):
-        label = filepath.stem.removeprefix('estimates_')
+    if filepath.stem.startswith('estimates_final_'):
+        label = filepath.stem.removeprefix('estimates_final_')
         with open(filepath, 'rb') as file:
             results[label] = pickle.load(file)
     
-#%%
-
-"""
-loading data for 1 participant
-"""
-
-
-# setting working directory 
-epoch_data_path = 'C:/Scriptie_Data/rps' # TEST: '/gedai_processed_data'
-
-# reading .fif filenames in the directory
-
-files = [f for f in os.listdir(epoch_data_path) if f.endswith('.fif')]
-s1_p1_file = epoch_data_path + "/" + files[0]
-
-p1_name = os.path.splitext(s1_p1_file)[0]
-
-
-# sampling frequency
-sfreq = 200
-
-# load player 1 data
-epoch_data_p1 = hmp.io.read_mne_data(
-    s1_p1_file, data_format='epochs', sfreq=sfreq,
-    verbose=True, subj_name=p1_name
-)
-
-
-# Add a common 'rt' coordinate to each dataset pointing to the right column
-"""use p2 rt as this is swapped in the eeg data"""
-
-epoch_data_p1 = epoch_data_p1.assign_coords(
-    rt=epoch_data_p1['player2_rt']
-)
-
-
-
-## CHECK FOR MISSING TRIALS------------------
-# Check per-participant trial counts after loading, before concat
-print("P1 participants:", epoch_data_p1.participant.values)
-print("P1 n_trials attr:", epoch_data_p1.attrs['n_trials'])
-
-# Also check for NaN epochs in the RT coordinates
-print("NaNs in p1 rt:", np.isnan(epoch_data_p1.data.values).sum())
-##----------------------------------------
-
-
-
-# %%
-
-"""
-fitting the model pipeline for participant 1
-"""
-
-# Step 1: Preprocess (set n_comp manually, more is more detailed, therefore also more likely to introduce noise)
-# preprocessed = hmp.preprocessing.Standard(epoch_data, n_comp=32, apply_standard=True)
-preprocessed = hmp.transformers.ProjPCA(epoch_data_p1, n_comp=32, interval_id='rt', offset_end=0.1, reject_threshold=1e-4, min_duration=0.2, max_duration=2.0)
-
-# Step 2: Create expected pattern
-pattern = hmp.patterns.HalfSine.create_expected(sfreq=epoch_data_p1.sfreq, width=50, location=50)
-    
-# Step 3: Build TrialData
-trial_data = hmp.trialdata.TrialData.from_transformer(preprocessed, pattern=pattern.template)
-
-# Step 4: Fit model (start with n_events=4, or loop over values for model comparison)
-n_events = 4
-#model = hmp.models.EventModel(pattern, n_events=n_events, starting_points=10, max_scale=51)
-model = hmp.models.CumulativeMethod(pattern)
-fitted_model, estimates = model.fit_transform(trial_data)
-
-#%%
-"""
-visualize data - topographies for participant 1
-"""
-# set working dir (not sure why this is necessary, maybe caused by read_info())
-os.chdir('C:/Scriptie_Data/rps') # TEST: '/gedai_processed_data'
-
-info = read_info(all_files[0], verbose=False)
-hmp.visu.plot_topo_timecourse(epoch_data_p1, estimates, info, as_time=True)
-
-
-#%%
-"""
-visualize data - timeline average probability for each event at each time sample for participant 1
-"""
-
-for event in range(5):
-    plt.plot(estimates.sel( event=event).mean('trial'), label=f"Event{event+1}")
-plt.ylabel('P(event)')
-plt.xlabel('Time (sample)')
-plt.legend()
-plt.show()
